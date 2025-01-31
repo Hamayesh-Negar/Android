41f7c57954815b717f649ffc5900abe0e0f03b47
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-31 20:27:29 +0330

    feat: implement ApiClient class for HTTP requests and update dependencies

---
+++ lib/services/api_client.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  static const String baseUrl = 'localhost:8000';
+  late Dio _dio;
+  String? _token;
+
+  ApiClient() {
+    _dio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      responseType: ResponseType.json,
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 5),
+    ));
+
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        if (_token != null) {
+          options.headers['Authorization'] = 'Token $_token';
+        }
+        return handler.next(options);
+      },
+    ));
+  }
+
+  void setToken(String token) {
+    _token = token;
+  }
+
+  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) async {
+    return await _dio.get(path, queryParameters: queryParameters);
+  }
+
+  Future<Response> post(String path, {dynamic data}) async {
+    return await _dio.post(path, data: data);
+  }
+
+  Future<Response> put(String path, {dynamic data}) async {
+    return await _dio.put(path, data: data);
+  }
+}
